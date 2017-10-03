69cb52f6c6102dc619ab7dca609de16d02bf6bcb
Nicolas <nico2che@gmail.com>
2017-10-03 16:47:21 -0400

    0.2 add caption with imagemagick, remove jq

--- README.md
+++ README.md
@@ -2,11 +2,15 @@
 
 Get bing's image of the day on your desktop and travel easily through the world all days
 
-**Setup dependencies**
+**Optionnal dependencies setup**
 
-`brew install jq`
+If you want a caption on your wallpaper to know more about it, you need the _imagemagick_ lib on your Mac.
 
-TODO: best script without any dependecies
+You can install _imagemagick_ with [Homebrew](https://brew.sh/)
+
+`brew install imagemagick`
+
+OR you can install _imagemagick_ with [the official way](https://www.imagemagick.org/script/download.php#macosx)
 
 **Setup script**
 
@@ -15,12 +19,12 @@ cd ~/Pictures
 git clone https://github.com/nico2che/macosx-bing-wallpaper.git wallpaper
 cd wallpaper
 chmod +x ./update.sh
-./update.sh
+./update.sh -n
 ```
 
 Or one liner:
 
-`cd ~/Pictures && git clone https://github.com/nico2che/macosx-bing-wallpaper.git wallpaper && cd wallpaper && chmod +x ./update.sh && ./update.sh`
+`cd ~/Pictures && git clone https://github.com/nico2che/macosx-bing-wallpaper.git wallpaper && cd wallpaper && chmod +x ./update.sh && ./update.sh -n`
 
 **Setup wallpaper**
 
@@ -29,10 +33,23 @@ then click on the only one wallpaper
 
 **Setup automation**
 
-In order to change your wallpaper all days, let's write a cron job
+In order to change your wallpaper all days, let's write a cron job which verify all hours
 
 `crontab -e`
 
 where you can write
 
-`0 * * * * ~/Pictures/wallpaper/update.sh`
+`0 * * * * ~/Pictures/wallpaper/update.sh [options]`
+
+### Options
+
+```
+	
+	-l --language			Set language code (eg. en-GB, fr-FR ...)
+							This option change the hour of new bing image, and the
+							caption language
+	-c --caption 			Write description on the image
+	-n --no-refresh			Do not refresh Dock (so, wallpaper) after downloaded it
+	--force					Force update image, even if already updated same day
+
+```
--- update.sh
+++ update.sh
@@ -1,19 +1,57 @@
 #!/bin/bash
 
+# macOS Bing's wallpaper
+# v0.2
+
+# Default language
 LANGUAGE="fr-CA"
+
+while [[ $# -gt 0 ]] ; do
+    arg="$1";
+    shift;
+    case "$arg" in
+        "-c"|"--caption" ) CAPTION=true;;
+        "--force" ) FORCE=true;;
+        "-n"|"--no-refresh" ) NOREFRESH=true;;
+        "-l"|"--language" ) LANGUAGE="$1"; shift;;
+        "--language="* ) LANGUAGE="${opt#*=}";;
+        *) >&2 echo "Unknown parameter: %arg\n"; exit 1;;
+    esac
+done
+
+# Bing's API URL
 API="http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=$LANGUAGE"
 
-JSON=$(curl -s $API)
-DATE=$(echo $JSON | /usr/local/bin/jq --raw-output '.images[0].fullstartdate')
+function parse {
+	echo $JSON | grep -Eo "\"$1\":\".*?\"" | sed -e "s/\"$1\":\"\([^\"]*\)\"/\1/";
+}
 
-if [ -f "$DATE.wall" ]; then
+JSON=$(curl -s $API)
+DATE=$(parse fullstartdate);
+
+if [ ! $FORCE ] && [ -f "$DATE.wall" ]; then
 	echo "Already up to date."
 else
 	echo "Updating..."
-	URL=$(echo $JSON | /usr/local/bin/jq --raw-output '.images[0].urlbase')
-	curl -s "https://www.bing.com{$URL}_1920x1080.jpg" -o wallpaper.jpg
-	killall Dock
+
+	URL=$(parse url);
+	curl -s "http://www.bing.com$URL" -o wallpaper.jpg
+
+	if [ $CAPTION ]; then
+		if [ $(type -t convert) ]; then
+			TEXT=$(parse copyright | sed -e 's/\( (©.*\)$//');
+			convert -background '#0008' -fill white -gravity center -size $((${#TEXT}*10))x40 -pointsize 20 \
+          			caption:"$TEXT" \
+          			wallpaper.jpg +swap -geometry +130+100 -gravity southeast -composite wallpaper.jpg;
+        else
+        	echo "! Warning: cannot add caption, you need to install imagemagick dependencies. See Homebrew and 'brew install imagemagick'";
+        fi
+    fi
+
 	rm *.wall
+	if [ ! $NOREFRESH ]; then
+		killall Dock
+	fi
 	touch "$DATE.wall"
-	echo "OK !"
+	echo "Updated!"
 fi
